34e775d435017e3adea8c9181cc1884ea5637d0b
mikesealey <mikesealey@hotmail.com>
2023-02-04 12:28:19 +0000

    Explores how colours can be used.

--- Mastermind.rb
+++ Mastermind.rb
@@ -1,1 +1,24 @@
 # Mastermind
+require 'colorize'
+require 'colorized_string'
+=begin
+    Computer randomly selects from 6 colours, four times
+
+    Red 
+    Orange
+    Yellow
+    Green
+    Light Blue
+    Blue
+    Purple
+
+=end
+
+#puts ColorizedString.colors
+
+puts "###RED###".colorize(:color => :white, :background => :red)
+puts "###PINK##".colorize(:color => :white, :background => :light_magenta)
+puts "#ORANGE##".colorize(:color => :white, :background => :yellow)
+puts "##GREEN##".colorize(:color => :white, :background => :green)
+puts "##BLUE###".colorize(:color => :white, :background => :blue)
+puts "##PURPLE#".colorize(:color => :white, :background => :magenta)
